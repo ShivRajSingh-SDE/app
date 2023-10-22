d8b63f5db1e6a27fc26cc769cc06c45bd9908e2e
Master_shiv.info.me <103109048+shivamkumarsha@users.noreply.github.com>
2023-10-23 00:53:47 +0530

    first commit

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -34,7 +34,7 @@ const HomePage = () => {
         <Contact />
         <div className="fixed top-0 left-0 z-50 pointer-events-none">
           <div
-            className="star-tracker absolute  rounded-full"
+            className=" hidden md:flex star-tracker absolute  rounded-full"
             style={{
               top: `${mousePosition.y}px`,
               left: `${mousePosition.x}px`,
